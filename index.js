b87fdf52a62320d865e2721dd98188a6244e1762
Michael Warkentin <michael@sentry.io>
2022-03-27 12:01:49 -0400

    implement simple divider api

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import { api } from "@serverless/cloud";
 api.get("/", async (req, res) => {
   res.send("<h1>Hello Serverless Cloud!</h1>");
 });
+
+api.get('/:dividend/dividedby/:divisor', (req,res) => {
+  let dividend = parseInt(req.params.dividend);
+  let divisor = parseInt(req.params.divisor);
+
+  console.log(dividend, divisor);
+
+  if (divisor === 0 || isNaN(dividend) || isNaN(divisor)) {
+    throw new Error(dividend + " / " + divisor);
+  }
+
+  let result = dividend / divisor;
+
+  res.send(`${result}`);
+});
